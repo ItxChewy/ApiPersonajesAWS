02312cbc5f28ac521045ad093d543a52c5b323f6
AlexRR03 <alexciempo03@gmail.com>
2025-05-13 11:11:13 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Program.cs
@@ -0,0 +1,35 @@
+using ApiPersonajesAWS.Context;
+using ApiPersonajesAWS.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddTransient<RepositoryPersonajes>();
+builder.Services.AddDbContext<PersonajesContext>
+    (options => options.UseMySQL(builder.Configuration.GetConnectionString("MySql")));
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+   
+}
+app.MapScalarApiReference();
+app.MapOpenApi();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ApiPersonajesAWS/Repositories/RepositoryPersonajes.cs
@@ -0,0 +1,43 @@
+﻿using ApiPersonajesAWS.Context;
+using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Repositories
+{
+    public class RepositoryPersonajes
+    {
+        private PersonajesContext context;
+
+        public RepositoryPersonajes(PersonajesContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            return await this.context.Personajes.ToListAsync();
+        }
+
+        public async Task<Personaje>GetPersonajeAsync(int id)
+        {
+            return await this.context.Personajes
+                .FirstOrDefaultAsync(x => x.IdPersonaje == id);
+        }
+
+        public async Task<int> GetMaxIdAsync()
+        {
+            return await this.context.Personajes.MaxAsync(x => x.IdPersonaje) + 1;
+        }
+
+        public async Task CreatePersonajesAsync(string nombre, string imagen)
+        {
+            Personaje p = new Personaje();
+            p.IdPersonaje = await this.GetMaxIdAsync();
+            p.Nombre = nombre;
+            p.Imagen = imagen;
+            await this.context.Personajes.AddAsync(p);
+            await this.context.SaveChangesAsync();
+        }
+    }
+
+}
