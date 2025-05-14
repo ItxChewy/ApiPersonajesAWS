46f5281e751c8696c92c83b75785c64ef172794f
ItxChewy <aarongarcia1906@gmail.com>
2025-05-14 10:46:52 +0200

    con el find

--- ApiPersonajesAWS/Controllers/PersonajesController.cs
+++ ApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -20,15 +20,21 @@ namespace ApiPersonajesAWS.Controllers
         {
             return await this.repo.GetPersonajesAsync();
         }
+        [HttpGet]
+        [Route("[action]/{id}")]
+        public async Task<ActionResult<Personaje>> GetPersonaje(int id)
+        {
+            return await this.repo.GetPersonajeAsync(id);
+        }
         [HttpPost]
         public async Task<ActionResult> Create(Personaje personaje)
         {
-            await this.repo.CreatePersonajesAsync(personaje.Nombre , personaje.Imagen);
+            await this.repo.CreatePersonajesAsync(personaje.Nombre, personaje.Imagen);
             return Ok();
         }
 
         [HttpPut]
-        public async Task<ActionResult>Update(Personaje personaje)
+        public async Task<ActionResult> Update(Personaje personaje)
         {
             await this.repo.UpdatePersonajeAsync(personaje.IdPersonaje, personaje.Nombre, personaje.Imagen);
             return Ok();
